308a65f2921ccdf011bdb543b26d66e3e7f527b2
hsinpa <hsinpa@gmail.com>
2020-11-30 12:07:27 +0800

    Add modals

---
+++ Assets/Hsinpa/Script/MVC/View/Modal/Component/AnchorEditModal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lighthouse.View {
+
+
+}
+public class AnchorEditModal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
