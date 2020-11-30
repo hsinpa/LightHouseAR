c575f74ffcc8339f5c3f183f2e7bdeb0592b614a
hsinpa <hsinpa@gmail.com>
2020-11-30 19:39:06 +0800

    Add modern ui component

---
+++ Assets/Hsinpa/Script/MVC/View/Modals/Modal/AnchorEditorModal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Michsky.UI.ModernUIPack;
+
+namespace Hsinpa.View
+{
+    public class AnchorEditorModal : Modal
+    {
+        [SerializeField]
+        Text semiInfoText;
+
+        [SerializeField]
+        ButtonManagerBasic closeBtn;
+
+        [SerializeField]
+        ButtonManagerBasic confirmBtn;
+
+    }
+}
